ff470067db8bc3ee84091d3cd01443305e830070
Akashprivacy <akashchoudhury90640@gmail.com>
2025-07-26 01:00:38 +0530

    Update server.ts

--- backend/server.ts
+++ backend/server.ts
@@ -1,6 +1,4 @@
-
-import express from 'express';
-import type { Request, Response } from 'express';
+import express, { Request, Response } from 'express';
 import puppeteer, { type Cookie, type Page, type Frame, Browser } from 'puppeteer';
 import cors from 'cors';
 import dotenv from 'dotenv';
